39f296967439fdd902b0dc19f53714c0fdeffda3
Juan Pablo <jpcruzgarcia@hotmail.com>
2024-01-14 18:18:07 -0600

    refactor

--- README.md
+++ README.md
@@ -2,39 +2,43 @@
 
 ## Descripción
 
-Este script de Python está diseñado para interactuar con canales y grupos de Telegram utilizando la biblioteca Telethon. Proporciona funcionalidad para descargar mensajes, especialmente aquellos con medios, de canales o grupos de Telegram especificados. Además, ofrece características para listar usuarios en un canal o grupo y manejar errores durante el proceso de descarga.
+Este script de Python está diseñado para interactuar con canales y grupos de Telegram utilizando la biblioteca Telethon. Permite la descarga de mensajes, especialmente aquellos con medios (como videos e imágenes), de canales o grupos de Telegram especificados. Además, ofrece funcionalidades para listar diálogos y manejar errores durante el proceso de descarga.
 
 ## Características
 
 -   Descarga de mensajes con medios de canales y grupos de Telegram.
 -   Registro de mensajes descargados para evitar duplicados.
 -   Registro de errores para descargas fallidas.
--   Listado de todos los canales y grupos a los que pertenece el usuario.
--   Listado de usuarios en un canal o grupo específico.
+-   Listado de todos los canales, grupos y usuarios con los que el cliente interactúa.
 -   Manejo de descargas simultáneas para optimizar el rendimiento.
+-   Uso de semáforos para controlar el número máximo de descargas simultáneas.
+-   Guarda los registros de descargas y errores en archivos de texto separados.
 
 ## Requisitos
 
 -   Python 3.6 o superior.
--   Biblioteca Telethon.
+-   Bibliotecas: Telethon, pytz, tqdm.
 -   Un ID de API de Telegram y hash de API válidos. https://my.telegram.org/apps
--   Archivo `config.json` que contenga el ID de API de Telegram y el hash de API.
+-   Archivo config.json que contenga el ID de API de Telegram, el hash de API y opcionalmente el número máximo de descargas simultáneas.
 
 ## Configuración
 
 1. Asegúrate de que Python 3.6 o superior esté instalado.
-2. Instala Telethon:
+2. Asegúrate de tener pip, el gestor de paquetes de Python, instalado en tu sistema.
+3. Asegúrate de tener pip, el gestor de paquetes de Python, instalado en tu sistema.
     ```bash
-    pip install telethon
+    pip install -r requirements.txt
     ```
-3. Crea un archivo `config.json` en el mismo directorio que el script con el siguiente contenido:
+    Esto instalará todas las dependencias listadas en el archivo requirements.txt.
+4. Crea un archivo config.json en el mismo directorio que el script con el siguiente contenido:
     ```json
     {
         "TELEGRAM_API_ID": "tu_api_id",
-        "TELEGRAM_API_HASH": "tu_api_hash"
+        "TELEGRAM_API_HASH": "tu_api_hash",
+        "MAX_SIMULTANEOUS_DOWNLOADS": 5
     }
     ```
-    Reemplaza `tu_api_id` y `tu_api_hash` con tus credenciales de API de Telegram.
+    Reemplaza tu_api_id y tu_api_hash con tus credenciales de API de Telegram y ajusta MAX_SIMULTANEOUS_DOWNLOADS según sea necesario.
 
 ## Uso
 
@@ -42,20 +46,7 @@ Este script de Python está diseñado para interactuar con canales y grupos de T
     ```bash
     python media.py
     ```
-2. Sigue las indicaciones interactivas para:
-    - Descargar mensajes de un canal/grupo elegido.
-    - Listar usuarios en un canal/grupo específico.
-
-## Funciones
-
--   `get_current_time()`: Devuelve la hora actual en una zona horaria predefinida.
--   `was_downloaded(message_id, log_file)`: Verifica si un mensaje ya fue descargado.
--   `log_error(message_id, error_file)`: Registra un error para un mensaje.
--   `download_message(message, download_directory, log_file, error_file)`: Descarga el mensaje especificado.
--   `process_errors(error_file, log_file, download_directory, channel_id)`: Procesa errores registrados durante las descargas.
--   `list_channels_and_groups()`: Lista todos los canales y grupos a los que pertenece el usuario.
--   `list_users(channel)`: Lista los usuarios en un canal o grupo especificado.
--   `main()`: La función principal que inicia el cliente y maneja las entradas del usuario.
+2. Sigue las indicaciones interactivas para elegir un canal o grupo y comenzar la descarga de mensajes. También puedes listar los canales, grupos y usuarios.
 
 ## Notas
 
--- media.py
+++ media.py
@@ -1,65 +1,76 @@
-from telethon import TelegramClient, errors
-from telethon.tl.functions.channels import GetParticipantsRequest
-from telethon.tl.types import ChannelParticipantsSearch
+from telethon import TelegramClient
 import asyncio
+import uuid
 import os
 import datetime
 import pytz
 import json
+from tqdm.asyncio import tqdm_asyncio
 
 with open('config.json', 'r') as config_file:
     config = json.load(config_file)
     api_id = config['TELEGRAM_API_ID']
     api_hash = config['TELEGRAM_API_HASH']
+    MAX_SIMULTANEOUS_DOWNLOADS = config.get('MAX_SIMULTANEOUS_DOWNLOADS', 5) 
+
 
 timezone = pytz.timezone('America/Mexico_City')
 
 client = TelegramClient('session_name', api_id, api_hash)
 
-MAX_SIMULTANEOUS_DOWNLOADS = 10
+semaphore = asyncio.Semaphore(MAX_SIMULTANEOUS_DOWNLOADS)
+
+downloaded_messages = set()
+error_messages = set()
 
 def get_current_time():
-    """Obtiene la hora actual en la zona horaria especificada."""
     return datetime.datetime.now(timezone).strftime('%Y-%m-%d %H:%M:%S')
 
-def was_downloaded(message_id, log_file):
-    """Verifica si un mensaje ya fue descargado previamente."""
-    with open(log_file, 'a+') as file:
-        file.seek(0)
-        downloaded = file.read().splitlines()
-        if str(message_id) in downloaded:
-            print(f"{get_current_time()} - Mensaje {message_id} ya fue descargado.")
-            return True
-        else:
-            file.write(str(message_id) + '\n')
-            return False
-
-def log_error(message_id, error_file):
-    """Registra un error en el archivo de errores."""
-    with open(error_file, 'a') as file:
-        file.write(str(message_id) + '\n')
-    print(f"{get_current_time()} - Error registrado para el mensaje {message_id}.")
-
 async def download_message(message, download_directory, log_file, error_file):
-    """Descarga un mensaje del canal o grupo."""
-    try:
-        print(f"{get_current_time()} - Iniciando descarga: {message.id}")
-        file_name = f"{message.date.strftime('%Y-%m-%d_%H-%M-%S')}_{message.id}"
-        path = await message.download_media(file=download_directory + '/' + file_name)
-        print(f"{get_current_time()} - Descargado: {path}")
-    except Exception as e:
-        print(f"{get_current_time()} - Error al descargar el mensaje {message.id}: {e}")
-        log_error(message.id, error_file)
+    async with semaphore:
+        temp_file_name = f"temp_{uuid.uuid4()}.part"
+        temp_file_path = os.path.join(download_directory, temp_file_name)
+    
+        extension = ''
+        if message.file and message.file.ext:
+            extension = message.file.ext
+        final_file_name = f"{message.date.strftime('%Y-%m-%d_%H-%M-%S')}_{message.id}{extension}"
+        final_file_path = os.path.join(download_directory, final_file_name)
+
+        try:
+            if message.file and message.file.size:
+                progress_bar = tqdm_asyncio(total=message.file.size, desc=final_file_name)
+
+                def progress_callback(current_bytes, total_bytes):
+                    progress_bar.n = current_bytes
+                    progress_bar.refresh()
+
+                await message.download_media(file=temp_file_path, progress_callback=progress_callback)
+                progress_bar.close()
+
+                if os.path.exists(temp_file_path):
+                    os.rename(temp_file_path, final_file_path)
+                    print(f"{get_current_time()} - Descargado: {final_file_path}")
+                    downloaded_messages.add(str(message.id))
+                    with open(log_file, 'a') as file:
+                        file.write(str(message.id) + '\n')
+                else:
+                    print(f"{get_current_time()} - No se encontró el archivo temporal: {temp_file_path}")
+            else:
+                print(f"{get_current_time()} - Mensaje {message.id} no tiene un archivo para descargar.")
+        except Exception as e:
+            if os.path.exists(temp_file_path):
+                os.remove(temp_file_path)
+            print(f"{get_current_time()} - Error al descargar el mensaje {message.id}: {e}")
+            error_messages.add(str(message.id))
+            with open(error_file, 'a') as file:
+                file.write(str(message.id) + '\n')
 
 async def process_errors(error_file, log_file, download_directory, channel_id):
-    """Procesa los mensajes que fallaron en la descarga anteriormente."""
     print(f"{get_current_time()} - Iniciando procesamiento de errores...")
-    with open(error_file, 'r') as file:
-        error_message_ids = file.read().splitlines()
-
-    for message_id in error_message_ids:
-        if was_downloaded(message_id, log_file):
-            print(f"{get_current_time()} - Mensaje {message_id} ya fue procesado anteriormente.")
+    
+    for message_id in error_messages:
+        if message_id in downloaded_messages:
             continue
 
         message = await client.get_messages(channel_id, ids=int(message_id))
@@ -68,85 +79,57 @@ async def process_errors(error_file, log_file, download_directory, channel_id):
         else:
             print(f"{get_current_time()} - Mensaje {message_id} no encontrado.")
 
-async def list_channels_and_groups():
-    """Lista los canales y grupos del usuario y devuelve un diccionario con los IDs."""
-    print("Canales y grupos:")
+async def list_dialogs():
+    print("Canales, grupos y conversaciones con usuarios:")
     contador = 1
-    channel_dict = {}
+    dialog_dict = {}
     async for dialog in client.iter_dialogs():
-        if dialog.is_channel or dialog.is_group:
-            nombre_canal = dialog.name.encode('utf-8', 'ignore').decode('utf-8')
-            print(f"id:{contador}, Nombre: {nombre_canal}, ID: {dialog.id}, Tipo: {'Canal' if dialog.is_channel else 'Grupo'}")
-            channel_dict[contador] = dialog.id
+        if dialog.is_channel or dialog.is_group or dialog.is_user:
+            nombre_dialog = dialog.name.encode('utf-8', 'ignore').decode('utf-8')
+            tipo_dialog = 'Canal' if dialog.is_channel else 'Grupo' if dialog.is_group else 'Usuario'
+            print(f"id:{contador}, Nombre: {nombre_dialog}, ID: {dialog.id}, Tipo: {tipo_dialog}")
+            dialog_dict[contador] = dialog.id
             contador += 1
-    return channel_dict
-
-async def list_users(channel):
-    """Lista los usuarios de un canal o grupo."""
-    offset_user = 0
-    limit_user = 100
-
-    try:
-        while True:
-            participants = await client(GetParticipantsRequest(
-                channel,
-                ChannelParticipantsSearch(''),
-                offset_user,
-                limit_user,
-                hash=0
-            ))
-            if not participants.users:
-                break
-            for user in participants.users:
-                print(f"ID: {user.id} - Nombre: {user.first_name} {' ' + user.last_name if user.last_name else ''} - @{user.username if user.username else 'Sin username'} - Estado: {user.status}")
-            offset_user += len(participants.users)
-    except errors.ChatAdminRequiredError:
-        print("Error: Privilegios de administrador requeridos para listar usuarios en este canal o grupo.")
+    return dialog_dict
 
 async def main():
-    await client.start()
+    try:
+        await client.start()
+        dialog_dict = await list_dialogs()
 
-    channel_dict = await list_channels_and_groups()
+        selected_id = int(input("Elige el número del canal, grupo o conversación con usuario para descargar los mensajes: "))
+        if selected_id in dialog_dict:
+            entity_id = dialog_dict[selected_id]
+            entity = await client.get_entity(entity_id)
 
-    print("\n¿Qué te gustaría hacer a continuación?")
-    print("1. Descargar mensajes de un canal o grupo")
-    print("2. Listar usuarios de un canal o grupo específico")
-    option_ = input("Elige una opción (1 o 2): ")
-
-    if option_ == '1':
-        selected_id = int(input("Ingresa el número del canal o grupo: "))
-        if selected_id in channel_dict:
-            channel_id = channel_dict[selected_id]
-            channel = await client.get_entity(channel_id)
-
-            download_directory = f"Downloads_{channel_id}"
-            log_file = f"downloads_log_{channel_id}.txt"
-            error_file = f"errors_log_{channel_id}.txt"
+            download_directory = f"Downloads_{entity_id}"
+            log_file = f"downloads_log_{entity_id}.txt"
+            error_file = f"errors_log_{entity_id}.txt"
             os.makedirs(download_directory, exist_ok=True)
 
-            print(f"{get_current_time()} - Canal o grupo {channel.title} ({channel_id}) encontrado, comenzando descargas...")
+            print(f"{get_current_time()} - {entity.title if hasattr(entity, 'title') else entity.first_name} ({entity_id}) encontrado, comenzando descargas...")
             tasks = set()
-            async for message in client.iter_messages(channel):
-                if was_downloaded(message.id, log_file) or not message.media:
+            async for message in client.iter_messages(entity):
+                if str(message.id) in downloaded_messages or not message.media:
                     continue
                 task = asyncio.create_task(download_message(message, download_directory, log_file, error_file))
                 tasks.add(task)
                 if len(tasks) >= MAX_SIMULTANEOUS_DOWNLOADS:
                     _done, tasks = await asyncio.wait(tasks, return_when=asyncio.FIRST_COMPLETED)
             await asyncio.gather(*tasks)
-            await process_errors(error_file, log_file, download_directory, channel_id)
+            await process_errors(error_file, log_file, download_directory, entity_id)
             print(f"{get_current_time()} - Proceso de descarga completado.")
-        else:
-            print("Número de canal/grupo inválido.")
+    except KeyboardInterrupt:
+        print("Interrupción detectada, cerrando el programa...")
+        tasks = [t for t in asyncio.all_tasks() if t is not asyncio.current_task()]
+        for task in tasks:
+            task.cancel()
+        await asyncio.gather(*tasks, return_exceptions=True)
+        print("Todas las tareas pendientes han sido canceladas.")
+    finally:
+        await client.disconnect()
+        print("Cliente desconectado y programa terminado.")
 
-    elif option_ == '2':
-        selected_id = int(input("Ingresa el número del canal o grupo: "))
-        if selected_id in channel_dict:
-            channel_id = channel_dict[selected_id]
-            channel = await client.get_entity(channel_id)
-            await list_users(channel)
-        else:
-            print("Número de canal/grupo inválido.")
-
-with client:
-    client.loop.run_until_complete(main())
+if __name__ == "__main__":
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(main())
